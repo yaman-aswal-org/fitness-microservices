43584e29c5d388fd139af56b0f4ad1e1e4ca5d58
Yaman Aswal <Yaman.Aswal@q3infotech.net>
2026-01-14 17:24:20 +0530

    user service jpa setting updated.

--- common_exceptions_lib/src/main/java/com/fitness/common_exceptions_lib/models/ErrorCode.java
+++
@@ -1,9 +0,0 @@
-package com.fitness.common_exceptions_lib.models;
-
-public enum ErrorCode {
-    INTERNAL_ERROR,
-    VALIDATION_ERROR,
-    RESOURCE_NOT_FOUND,
-    UNAUTHORIZED,
-    FORBIDDEN
-}
